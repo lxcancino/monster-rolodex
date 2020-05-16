b4ebe2cb910fa7585bcc7f4207697b5cbec1975e
Ruben Cancino <rubencancino6@gmail.com>
2020-05-16 10:08:53 -0500

    Updates

---
+++ precache-manifest.2c60a649067586030eb68b1f3478b520.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9085fc7e6828d8d531945f935c85f97",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "672f80efb35b9d84d9a6",
+    "url": "/monster-rolodex/static/css/main.d70d9e41.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monster-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "672f80efb35b9d84d9a6",
+    "url": "/monster-rolodex/static/js/main.2dfa7f52.chunk.js"
+  },
+  {
+    "revision": "fdce5faec76031078f23",
+    "url": "/monster-rolodex/static/js/runtime-main.c9e8ecbd.js"
+  }
+]);
